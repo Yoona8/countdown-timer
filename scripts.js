1d82104be08d45cd1b76c9b8090c959d770c816e
Yoona8 <elena243v@gmail.com>
2020-08-07 15:10:27 +0300

    Add minutes

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const timerElement = document.querySelector('.timer');
 
+const formatTime = (time) => time.toString().padStart(2, '0');
+
 const runTimer = (time) => {
     let timeLeft = time;
 
@@ -8,7 +10,12 @@ const runTimer = (time) => {
             clearInterval(timer);
         }
 
-        timerElement.textContent = timeLeft;
+        let minutes = Math.floor(timeLeft % (60 * 60) / 60);
+        let seconds = Math.floor(timeLeft % 60);
+        let minOutput = formatTime(minutes);
+        let secOutput = formatTime(seconds);
+
+        timerElement.textContent = `${minOutput}:${secOutput}`;
         timeLeft--;
     }, 1000);
 };
