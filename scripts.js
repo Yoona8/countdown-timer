bd1638ff403b4c98a15f5db0e4b7dadf6a36cd92
Yoona8 <elena243v@gmail.com>
2020-09-07 18:53:49 +0300

    Add changing colors

--- scripts.js
+++ scripts.js
@@ -2,8 +2,17 @@ const timerElement = document.querySelector('.timer');
 
 const formatTime = (time) => time.toString().padStart(2, '0');
 
-const runTimer = (time) => {
+const Color = {
+    BASIC_BLUE: 'var(--basic-blue)',
+    BASIC_GOLD: 'var(--basic-gold)',
+    LIGHT_GOLD: 'var(--light-gold)'
+};
+
+const runTimer = ({time, timeColorOne, timeColorTwo}) => {
     let timeLeft = time;
+    let color = Color.LIGHT_GOLD;
+
+    timerElement.style.color = color;
 
     const timer = setInterval(() => {
         if (timeLeft <= 0) {
@@ -17,7 +26,25 @@ const runTimer = (time) => {
 
         timerElement.textContent = `${minOutput}:${secOutput}`;
         timeLeft--;
+
+        if (timeLeft > timeColorOne) {
+            return;
+        }
+
+        if (timeLeft < timeColorTwo) {
+            color = Color.BASIC_BLUE;
+        } else if (timeLeft < timeColorOne) {
+            color = Color.BASIC_GOLD;
+        }
+
+        timerElement.style.color = color;
     }, 1000);
 };
 
-runTimer(10);
+const config = {
+    time: 100,
+    timeColorOne: 60,
+    timeColorTwo: 30
+};
+
+runTimer(config);
